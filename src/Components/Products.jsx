7b2522af27531b40243172b37ef3929d4d8f5d8e
Hricheak <lincodeintern.12@lincode.ai>
2025-04-18 14:55:48 +0530

    Error handling done

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -30,16 +30,34 @@ const Products = () => {
   }, []);
 
   const allItems = useSelector((state) => state.cart.allItems);
+  console.log(allItems.length);
+  
 
-  return (
-    <>
-      <div className="flex flex-wrap gap-3 sm:px-16 px-3 justify-around items-center py-10">
-        {allItems?.map((item) => (
-          <ProductCard key={item.id} item={item} />
-        ))}
-      </div>
-    </>
-  );
+  if (allItems.length < 1) {
+    return (
+      <>
+        <div className="bg-gray-200 w-full h-44 flex flex-col items-center justify-center">
+          <p className="text-xl font-semibold text-gray-600 font-mono">
+            No Items Found
+          </p>
+
+          <p className="text-[10px] font-thin pt-1 text-gray-500">
+            Check your Internet Conection.
+          </p>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="     flex flex-wrap gap-3 sm:px-16 px-3 justify-around items-center py-10">
+          {allItems?.map((item) => (
+            <ProductCard key={item.id} item={item} />
+          ))}
+        </div>
+      </>
+    );
+  }
 };
 
 export default Products;
